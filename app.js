a1c3d3d2e1eba94a0f2afe38c945dc80e68aa224
Rnsaputra <rnsaputra12@gmail.com>
2025-02-11 09:27:34 +0700

    first commit

--- app.js
+++ app.js
@@ -12,7 +12,49 @@ document.addEventListener("DOMContentLoaded", function () {
         },
         autoplay: {
             delay: 5000,
-        },        
+        },
+        slidesPerView: 2, // 1 card terlihat di layar kecil
+        spaceBetween: 20, // Jarak antar card
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2
+            }, 
+            1024: {
+                slidesPerView: 4,
+                coverflowEffect: {
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 3,
+                    slideShadow: true
+                }
+            } 
+        },
+
+
+    });
+    new Swiper(".mySwiper2", {
+        grabcursor: true,
+        effect: "coverflow",
+        centeredSlides: true,
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 3,
+            slideShadow: true
+        },
+        autoplay: {
+            delay: 4500,
+        },
         slidesPerView: 2, // 1 card terlihat di layar kecil
         spaceBetween: 20, // Jarak antar card
         pagination: {
@@ -41,20 +83,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     });
-    new Swiper(".mySwiper2", {
+    new Swiper(".mySwiper3", {
         grabcursor: true,
-        effect: "coverflow",
+        
+        loop: true,
         centeredSlides: true,
-        coverflowEffect: {
+        flipEffect: {
             rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 3,
-            slideShadow: true
+            stretch: 80,
+            depth: 350,
+            modifier: 1,
+            slideShadow: true,
         },
         autoplay: {
-            delay: 4500,
-        },        
+            delay: 3000,
+        },
         slidesPerView: 2, // 1 card terlihat di layar kecil
         spaceBetween: 20, // Jarak antar card
         pagination: {
@@ -71,13 +114,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }, // 2 card terlihat di layar >= 640px
             1024: {
                 slidesPerView: 4,
-                coverflowEffect: {
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 3,
-                    slideShadow: true
-                }
+
             } // 3 card terlihat di layar >= 1024px
         },
 
